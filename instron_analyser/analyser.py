33191a4dd037ed7b03a049299a386f2cc4b3c5c1
Kieran Sonter <ksonter95@gmail.com>
2025-02-03 19:44:48 +1000

    Initial commit of working application

---
+++ instron_analyser/analyser.py
@@ -0,0 +1,201 @@
+import data
+import pandas as pd
+
+from pathlib import Path
+from typing import Optional
+
+
+class Analyser:
+    """
+    Base class for all data analysers.
+
+    Args:
+        input_csv: The path to the CSV file containing the output of the
+            Instron.
+        output_xlsx: The path to the Excel file which will contain the analysis
+            results.
+    """
+
+    def __init__(self, input_csv: Path, output_xlsx: Path) -> None:
+
+        self._processed_data: list[data.ProcessedData] = []
+
+        self.output_xlsx: Path = output_xlsx
+        self.raw_data: data.RawData = data.RawData(input_csv)
+        self.processed_data = []
+
+    @property
+    def processed_data(self) -> list[data.ProcessedData]:
+        return self._processed_data
+
+    @processed_data.setter
+    def processed_data(self, value: list[data.ProcessedData]) -> None:
+        self._processed_data = value
+
+    def analyse(self) -> None:
+        """
+        Analyses the processed data.
+        """
+
+        pass
+
+    def save(self) -> None:
+        """
+        Saves the analysis results as well as the raw and processed data to an
+        Excel file.
+        """
+
+        with pd.ExcelWriter(self.output_xlsx, engine="xlsxwriter") as writer:
+            # Save the raw data
+            self.raw_data.write_to_excel(writer)
+
+            # Save all the processed data
+            for d in self.processed_data:
+                d.write_to_excel(writer)
+
+            # Save all the analysis results
+            # TODO
+
+
+class RelaxationAnalyser(Analyser):
+    """
+    The analyser used to analyse the Instron output for the relaxation stress
+    test.
+
+    Args:
+        input_csv: The path to the CSV file containing the output of the
+            Instron.
+        output_xlsx: The path to the Excel file which will contain the analysis
+            results.
+        relaxation_intervals: The number of relaxation intervals within the raw
+            data to include in the analysis.  This is used in combination with
+            relaxation_step_pct to determine the relaxation strains to include
+            in the analysis.  For instance, for relaxation_intervals = 6 and
+            relaxation_step_pct = 5, the analysis will include the strains
+            [5, 10, 15, 20, 25, 30].
+        relaxation_step_pct: The difference between consecutive relaxation
+            strains to use in the analysis.  This is used in combination with
+            relaxation_intervals to determine the relaxation strains to include
+            in the analysis.  For instance, for relaxation_intervals = 6 and
+            relaxation_step_pct = 5, the analysis will include the strains
+            [5, 10, 15, 20, 25, 30].
+        epsilon_pct: Tolerance for the strain within which it is considered to
+            be maintained for each relaxation interval.
+        regression_data_points: Maximum number of data points to be included in
+            the regression analysis.
+    """
+
+    def __init__(
+        self,
+        input_csv: Path,
+        output_xlsx: Path,
+        relaxation_intervals: int = 6,
+        relaxation_step_pct: float = 5.0,
+        epsilon_pct: float = 0.01,
+        regression_data_points: Optional[int] = None,
+    ) -> None:
+
+        super().__init__(input_csv, output_xlsx)
+
+        self._relaxation_intervals: int = 0
+        self._relaxation_step_pct: float = 0.0
+        self._relaxation_strains_pct: list[float] = []
+
+        self.relaxation_intervals = relaxation_intervals
+        self.relaxation_step_pct = relaxation_step_pct
+        self.epsilon_pct: float = epsilon_pct
+        self.regression_data_points: Optional[int] = regression_data_points
+
+    def __str__(self) -> str:
+
+        summary = f"{self.output_xlsx.name}\n"
+        summary += "Equation: F = a * e^(-t/tau) + b\n"
+        summary += "\n"
+
+        for i in range(self.relaxation_intervals):
+            summary += f"Strain (%) = {self.relaxation_strains_pct[i]}\n"
+            summary += f" - a   = {self.processed_data[i].a}\n"
+            summary += f" - b   = {self.processed_data[i].b}\n"
+            summary += f" - tau = {self.processed_data[i].tau}\n"
+
+        summary += "\n"
+
+        return summary
+
+    @property
+    def processed_data(self) -> list[data.RelaxationData]:  # type: ignore
+        return super().processed_data  # type: ignore
+
+    @processed_data.setter
+    def processed_data(self, value: list[data.RelaxationData]) -> None:  # type: ignore
+        super().processed_data[:] = value
+
+    @property
+    def relaxation_intervals(self) -> int:
+        return self._relaxation_intervals
+
+    @relaxation_intervals.setter
+    def relaxation_intervals(self, value: int) -> None:
+        self._relaxation_intervals = value
+
+        # Resize the relaxation strains based on the required number of relaxation
+        # intervals
+        if len(self._relaxation_strains_pct) > self._relaxation_intervals:
+            del self._relaxation_strains_pct[self._relaxation_intervals :]
+        else:
+            offset_pct: float = (
+                self._relaxation_strains_pct[-1]
+                if len(self._relaxation_strains_pct) > 0
+                else 0
+            )
+            self._relaxation_strains_pct.extend(
+                [
+                    (i + 1) * self._relaxation_step_pct + offset_pct
+                    for i in range(
+                        self._relaxation_intervals - len(self._relaxation_strains_pct)
+                    )
+                ]
+            )
+
+        # Resize the relaxation data based on the required number of relaxation
+        # intervals
+        if len(self.processed_data) > self._relaxation_intervals:
+            del self.processed_data[self._relaxation_intervals :]
+        else:
+            self.processed_data.extend(
+                [
+                    data.RelaxationData(self.raw_data.raw_data_frame)
+                    for _ in range(
+                        self._relaxation_intervals - len(self.processed_data)
+                    )
+                ]
+            )
+
+    @property
+    def relaxation_step_pct(self) -> float:
+        return self._relaxation_step_pct
+
+    @relaxation_step_pct.setter
+    def relaxation_step_pct(self, value: float) -> None:
+        self._relaxation_step_pct = value
+
+        self._relaxation_strains_pct = [
+            (i + 1) * self._relaxation_step_pct
+            for i in range(self._relaxation_intervals)
+        ]
+
+    @property
+    def relaxation_strains_pct(self) -> list[float]:
+        return self._relaxation_strains_pct
+
+    def analyse(self) -> None:
+        """
+        Analyses the processed data.
+        """
+
+        for i in range(self.relaxation_intervals):
+            self.processed_data[i].process_raw_data(
+                self.relaxation_strains_pct[i],
+                self.epsilon_pct,
+                self.regression_data_points,
+            )
---
+++ instron_analyser/data.py
@@ -0,0 +1,385 @@
+import numpy as np
+import pandas as pd
+import warnings
+
+from pathlib import Path
+from scipy.optimize import curve_fit  # type: ignore
+from typing import Any, Optional
+
+
+class DataFrame(pd.DataFrame):
+    """
+    Wrapper for pd.DataFrame that allows the class to be interacted with like a
+    standard pd.DataFrame.
+
+    Args:
+        data_frame: pd.DataFrame to wrap.
+    """
+
+    def __init__(self, data_frame: pd.DataFrame) -> None:
+
+        self._data_frame: pd.DataFrame = data_frame.copy(deep=True)
+        self._data_frame.reset_index(drop=True, inplace=True)
+
+    def __getattr__(self, attribute: str) -> Any:
+        return getattr(self._data_frame, attribute)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name == "_data_frame":
+            object.__setattr__(self, name, value)  # prevent recursion
+        else:
+            setattr(self._data_frame, name, value)
+
+    def __getitem__(self, key: Any) -> Any:
+        return self._data_frame[key]  # type: ignore
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        self._data_frame[key] = value
+
+    def __str__(self) -> str:
+        return str(self._data_frame)
+
+    @property
+    def displacement(self) -> "pd.Series[float]":
+        return self._data_frame[("Displacement", "(mm)")]  # type: ignore
+
+    @property
+    def e_modulus(self) -> "pd.Series[float]":
+        return self._data_frame[("E-modulus", "(MPa)")]  # type: ignore
+
+    @property
+    def force(self) -> "pd.Series[float]":
+        return self._data_frame[("Force", "(N)")]  # type: ignore
+
+    @property
+    def initial_time_s(self) -> float:
+        return self.time[0]
+
+    @property
+    def strain(self) -> "pd.Series[float]":
+        return self._data_frame[("Strain", "(%)")]  # type: ignore
+
+    @property
+    def stress(self) -> "pd.Series[float]":
+        return self._data_frame[("Compressive Stress", "(MPa)")]  # type: ignore
+
+    @property
+    def time(self) -> "pd.Series[float]":
+        return self._data_frame[("Time", "(s)")]  # type: ignore
+
+    @staticmethod
+    def load(csv: Path) -> "DataFrame":
+        """
+        Loads the CSV file into a DataFrame and validates its contents.
+
+        Args:
+            csv: The path to the CSV file containing the data set.
+        """
+
+        try:
+            # Read the CSV fil
+            data_frame: pd.DataFrame = pd.read_csv(csv, header=None)  # type: ignore
+
+            # Validate the CSV file
+            #  - Data headings must be in rows 19-20, columns 0-4 (0-indexed) and
+            #    must be:
+            #       Time, Displacement, Force, Strain (%), Compressive stress
+            #       (s), (mm),          (N),   (%),        (MPa)
+            #  - Data must be in rows 21-... (0-indexed) and must be all floating
+            #    point numbers
+            headings: pd.DataFrame = pd.DataFrame(
+                [
+                    [
+                        "Time",
+                        "Displacement",
+                        "Force",
+                        "Strain (%)",
+                        "Compressive stress",
+                    ],
+                    ["(s)", "(mm)", "(N)", np.nan, "(MPa)"],
+                ]
+            )
+            if (
+                not np.array_equal(
+                    data_frame.iloc[19:21, :5].to_numpy(dtype=str),  # type: ignore
+                    headings.to_numpy(dtype=str),  # type: ignore
+                )
+                or not data_frame.iloc[21:, :5]  # type: ignore
+                .map(lambda x: pd.to_numeric(x, errors="coerce"))  # type: ignore
+                .notna()
+                .all()
+                .all()
+            ):
+                raise ValueError
+
+        except:
+            raise ValueError("Invalid CSV file")
+
+        # Extract the data frame from the CSV file
+        # NOTE: the data frame starts in row 22 (indexed from 1)
+        data_frame = data_frame.iloc[21:, :5].map(  # type: ignore
+            lambda x: pd.to_numeric(x, errors="coerce")  # type: ignore
+        )
+        data_frame.columns = pd.MultiIndex.from_tuples(  # type: ignore
+            [
+                ("Time", "(s)"),
+                ("Displacement", "(mm)"),
+                ("Force", "(N)"),
+                ("Strain", "(%)"),
+                ("Compressive stress", "(MPa)"),
+            ]
+        )
+        data_frame.reset_index(drop=True, inplace=True)
+
+        # Add the Young's modulus column
+        # E = σ / ε
+        #   - E = Young's modulus (in Pa)
+        #   - σ = stress (in Pa)
+        #   - ε = strain (unitless)
+        # NOTE: handling divide by zero runtime warning
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            data_frame[("E-modulus", "(MPa)")] = data_frame[
+                ("Compressive stress", "(MPa)")
+            ] / (data_frame[("Strain", "(%)")] / 100)
+
+        return DataFrame(data_frame)
+
+
+class RelaxationDataFrame(DataFrame):
+    """
+    Wrapper for pd.DataFrame that allows the class to be interacted with like a
+    standard pd.DataFrame.  It contains all the data from a relaxation test.
+
+    Args:
+        data_frame: pd.DataFrame to wrap.
+    """
+
+    def __init__(self, data_frame: pd.DataFrame) -> None:
+
+        super().__init__(data_frame)
+
+        # Initially populate the processed data columns with default values
+        self.relative_time = pd.Series([0.0] * self.index.size)  # type: ignore
+        self.regression_force = pd.Series([0] * self.index.size)  # type: ignore
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name in ["regression_force", "relative_time"]:
+            object.__setattr__(self, name, value)
+        else:
+            super().__setattr__(name, value)
+
+    @property
+    def regression_force(self) -> "pd.Series[float]":
+        return self._data_frame[("Regression Force", "(N)")]  # type: ignore
+
+    @regression_force.setter
+    def regression_force(self, value: "pd.Series[float]") -> None:
+        self._data_frame[("Regression Force", "(N)")] = value
+
+        # Move the regression force column to be directly after the force column
+        column: pd.Series = self._data_frame.pop(("Regression Force", "(N)"))  # type: ignore
+        self._data_frame.insert(  # type: ignore
+            self._data_frame.columns.get_loc(("Force", "(N)")) + 1,  # type: ignore
+            ("Regression Force", "(N)"),
+            column,
+        )
+
+    @property
+    def relative_time(self) -> "pd.Series[float]":
+        return self._data_frame[("Relative Time", "(s)")]  # type: ignore
+
+    @relative_time.setter
+    def relative_time(self, value: "pd.Series[float]") -> None:
+        self._data_frame[("Relative Time", "(s)")] = value
+
+        # Move the relative time column to be directly after the time column
+        column: pd.Series = self._data_frame.pop(("Relative Time", "(s)"))  # type: ignore
+        self._data_frame.insert(  # type: ignore
+            self._data_frame.columns.get_loc(("Time", "(s)")) + 1,  # type: ignore
+            ("Relative Time", "(s)"),
+            column,
+        )
+
+
+class Data:
+    """
+    Base class for all data.
+
+    Args:
+        raw_data_frame: The raw data frame which will be used to create the
+            processed data.
+        sheet_name: The name of the Excel sheet to which the processed data will
+            be written.
+    """
+
+    def __init__(self, raw_data_frame: DataFrame, sheet_name: str) -> None:
+
+        self.raw_data_frame: DataFrame = raw_data_frame
+        self.processed_data_frame: DataFrame = self.raw_data_frame
+        self.sheet_name: str = sheet_name
+
+    def __str__(self) -> str:
+        return str(self.processed_data_frame)
+
+    def write_to_excel(self, writer: pd.ExcelWriter) -> None:
+        self.processed_data_frame.drop(self.processed_data_frame.index).to_excel(  # type: ignore
+            writer,
+            sheet_name=self.sheet_name,
+            startrow=0,
+        )
+        self.processed_data_frame.to_excel(  # type: ignore
+            writer,
+            sheet_name=self.sheet_name,
+            startrow=1,
+            header=False,
+        )
+        # writer.sheets[self.sheet_name].set_row(2, None, None, {"hidden": True})
+
+    def process_raw_data(self, *args: list[Any], **kwargs: dict[str, Any]) -> None:
+        """
+        Processes the raw data read from the CSV output of the Instron.  This
+        could include populating additional columns or calculating summary
+        parameters of the dataset.
+        """
+
+        pass
+
+
+class RawData(Data):
+    """
+    Data storage class for the raw data obtained from the output of the Instron.
+
+    Args:
+        input_csv: The path to the CSV file containing the output of the
+            Instron.
+    """
+
+    def __init__(self, input_csv: Path) -> None:
+
+        super().__init__(DataFrame.load(input_csv), "Raw Data")
+
+
+class ProcessedData(Data):
+    """
+    Base class for all processed data.
+
+    Args:
+        raw_data_frame: The raw data frame which will be used to create the
+            processed data.
+        sheet_name: The name of the Excel sheet to which the processed data will
+            be written.
+    """
+
+    pass
+
+
+class RelaxationData(ProcessedData):
+    """
+    Data storage class for the data where the strain is maintained while the
+    sample relaxes.  Columns containing the relative time and force regression
+    are also calculated and added to the data.
+
+    Args:
+        raw_data_frame: The raw data frame which will be used to create the
+            processed data.
+    """
+
+    def __init__(
+        self,
+        raw_data_frame: DataFrame,
+    ) -> None:
+
+        super().__init__(raw_data_frame, "Strain = ?%")
+
+        # Regression equation parameters for equation y = a * e^(-t / tau) + b
+        self.a: float = 0.0
+        self.b: float = 0.0
+        self.tau: float = 1.0
+
+    def process_raw_data(  # type: ignore
+        self,
+        relaxation_strain_pct: float,
+        epsilon_pct: float,
+        regression_data_points: Optional[int],
+    ) -> None:
+        """
+        Processes the raw data read from the CSV output of the Instron.
+
+        Dataset filtering:
+            - Relaxation phase of the sample at the specified strain.
+
+        Columns that are populated:
+            - Relative Time: Time elapsed since the start of the relaxation
+                phase.
+            - Force Regression: Force at each relative time point as calculated
+                by the exponential decay regression equation of the force.
+
+        Summary parameters that are calculated:
+            - Exponential decay regression equation parameters (a, b, and tau in
+                y = a * e^(-t / tau) + b)
+
+        Args:
+            relaxation_strain_pct: The strain at which the sample is maintained
+                while the sample relaxes.
+            epsilon_pct: Tolerance for the strain within which it is considered
+                to be maintained.
+            regression_data_points: Maximum number of data points to be included
+                in the regression analysis.
+        """
+
+        self.sheet_name = f"Strain = {round(relaxation_strain_pct, 1)}%"
+
+        # Filter the raw data to only obtain the data within the relation phase
+        # of the sample for the specified strain
+        self.processed_data_frame = RelaxationDataFrame(
+            self.raw_data_frame[
+                (self.raw_data_frame.strain > (relaxation_strain_pct - epsilon_pct))
+                & (self.raw_data_frame.strain < (relaxation_strain_pct + epsilon_pct))
+            ]
+        )
+
+        # Add the relative time column
+        self.processed_data_frame.relative_time = (
+            self.processed_data_frame.time - self.processed_data_frame.initial_time_s
+        )
+
+        # Calculate the parameters of the exponential decay equation that best
+        # fits the data points
+        [self.a, self.b, self.tau], _ = curve_fit(  # type: ignore
+            self.y,
+            self.processed_data_frame.relative_time.head(  # type: ignore
+                regression_data_points
+                or self.processed_data_frame.index.size  # type: ignore
+            ),
+            self.processed_data_frame.force.head(  # type: ignore
+                regression_data_points
+                or self.processed_data_frame.index.size  # type: ignore
+            ),
+        )
+
+        # Add the regression force column directly after the force column
+        self.processed_data_frame.regression_force = pd.Series(
+            [
+                self.y(t, self.a, self.b, self.tau)  # type: ignore
+                for t in self.processed_data_frame.relative_time  # type: ignore
+            ]
+        )
+
+    @staticmethod
+    def y(t: float, a: float, b: float, tau: float) -> float:
+        """
+        Calculates the value of following equation:
+        y = a * e^(-t / tau) + b
+
+        Args:
+            t: Time at which the function is to be computed.
+            a: Amplitude of the exponential decay.
+            b: Baseline value of the function.
+            tau: Time constant of the exponential decay.
+
+        Returns:
+            Value of the function at time t.
+        """
+
+        return a * np.exp(-t / tau) + b
